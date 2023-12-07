2fa516c64a931bb4afe255bf221b9acc71e5db54
labradorescence <orangenewyorkcreativeagency@gmail.com>
2023-12-07 15:12:35 -0500

    prac

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
-function MainContainer() {
+function MainContainer({stocks}) {
+
+  const [myPortfolio, setMyPortfolio] =  useState([])
+
+  const onSetMyPortfolio = (newPortfolio) => {
+    let newMyPortfolio = myPortfolio.push(newPortfolio)
+    setMyPortfolio(myPortfolio.push(newPortfolio))
+  }
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={stocks} onSetMyPortfolio={onSetMyPortfolio}/>
         </div>
         <div className="col-4">
           <PortfolioContainer />
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({stocks,onSetMyPortfolio }) {
+
+  console.log("from container", stocks)
+
+  const eachStock = stocks.map((stock) => {
+    return <Stock key={stock.id} stock={stock} onSetMyPortfolio={onSetMyPortfolio} />
+  })
+
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {eachStock}
     </div>
   );
 }
